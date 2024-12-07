4f7440f4a58d1bada411d110756073be38ba8602
SurajPrasad9737 <surajprasad311004@gmail.com>
2024-12-07 06:40:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT||3000;
+const jokes = [
+    {
+        id:1,
+        title:"First joke",
+        content:"This is first joke content"
+    },
+    {
+        id:2,
+        title:"Second joke",
+        content:"This is second joke content"
+    },
+    {
+        id:3,
+        title:"Third joke",
+        content:"This is third joke content"
+    }];
+app.get('/',(req,res)=>{
+    res.send('Server is ready');
+})
+
+app.get('/api/jokes',(req,res)=>{
+    res.send(jokes);
+});
+
+app.listen(process.env.PORT,()=>{
+    console.log(`App is listening on http://localhost:${port}`);
+})
